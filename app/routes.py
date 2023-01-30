50ccde61058243ea14ade1bb7da9fb405cd43297
= <=>
2023-01-30 20:33:44 +0100

    add routes for static serving

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.controllers import UserController
+
+# Setting up routing this way allows for more flexibility
+# and allows to an experience similar to express(Node.js) router
+# interface
+def setup_routing(app: Flask):
+    app.add_url_rule('/', view_func=UserController.index_page, methods=['GET'])
+    app.add_url_rule('/users', view_func=UserController.fetch_users, methods=['GET'])
+    app.add_url_rule('/users/<string:page>',view_func=UserController.fetch_users, methods=['GET'])
+    pass
