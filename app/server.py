56ded24bd32603e507630fbd2ba39dcac58a2dab
= <=>
2023-01-30 20:34:54 +0100

    add migration utilities

--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import jinja_partials
 from .config import Config
 
@@ -18,6 +19,7 @@ if Config.FLASK_ENV == 'production':
 else:
     app.config["SQLALCHEMY_DATABASE_URI"] = Config.DB_URL
 
+migrate = Migrate(app, db)
 db.init_app(app)
 jinja_partials.register_extensions(app)
 def create_table():
